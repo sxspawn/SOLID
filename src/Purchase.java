7c2126ec3c5513674c81cf993072d6608a70e91d
Kovbasa Igor <kovbasa.i@yandex.ru>
2023-05-04 11:48:41 +0300

    add "Свойства хорошего кода и принцип SOLID" код в исходном виде, до начала правок

---
+++ src/Purchase.java
@@ -0,0 +1,40 @@
+import java.util.Map;
+
+public class Purchase {
+    protected String title;
+    protected int count;
+    protected Purchase[] purchases = new Purchase[4];
+
+    public Purchase(String title, int count) {
+        this.title = title;
+        this.count = count;
+    }
+
+    public Purchase() {
+    }
+
+    public void addPurchase(String title, int count) {
+        for (int i = 0; i < purchases.length; i++) {
+            if (purchases[i] == null) {
+                purchases[i] = new Purchase(title, count);
+                return;
+            }
+            if (purchases[i].title.equals(title)) {
+                purchases[i].count += count;
+                return;
+            }
+        }
+    }
+
+    public long sum(Map<String, Integer> prices) {
+        long sum = 0;
+        System.out.println("КОРЗИНА:");
+        for (int i = 0; i < purchases.length; i++) {
+            Purchase purchase = purchases[i];
+            if (purchase == null) continue;
+            System.out.println("\t" + purchase.title + " " + purchase.count + " шт. в сумме " + (purchase.count * prices.get(purchase.title)) + " руб.");
+            sum += purchase.count * prices.get(purchase.title);
+        }
+        return sum;
+    }
+}
